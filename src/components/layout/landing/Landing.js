418135bfde885b5a3ea13616801fd3cea1a10d94
James McCarthy <jamesmccarthyxci@gmail.com>
2020-01-04 13:33:19 -0500

    Finished Landing Page

--- src/components/layout/landing/Landing.js
+++ src/components/layout/landing/Landing.js
@@ -6,6 +6,7 @@ import image_3 from "../../../assets/img/landing/3.jpg";
 import image_5 from "../../../assets/img/landing/5.jpg";
 import image_6 from "../../../assets/img/landing/6.jpg";
 import image_7 from "../../../assets/img/landing/7.jpg";
+import logo from "../../../assets/img/logo.png";
 
 class Landing extends Component {
   render() {
@@ -13,46 +14,21 @@ class Landing extends Component {
       <div>
         <div className="pimg1">
           <div className="ptext">
-            <span className="border">Expensicon</span>
-          </div>
-        </div>
-
-        <div className="section section-light">
-          <div className="row">
-            <div className="col m6 offset-m3">
-              <h2>Section One</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad
-                provident laudantium exercitationem, dolorem sint soluta
-                quibusdam in iure, fugit magni distinctio rem expedita ab unde
-                odio quam voluptate nam voluptates, iusto doloribus dolorum
-                molestiae quidem! Odio harum omnis sint necessitatibus sed
-                corporis maiores pariatur nisi minima. Doloremque rerum ipsum
-                cumque totam nulla adipisci ducimus tempora ab ut voluptatum
-                minima dolore aperiam, possimus officiis accusamus repellat
-                tenetur, animi at expedita nam! Eligendi ducimus commodi quas
-                possimus, nemo aliquam vero corrupti dolor optio? Animi, quia
-                neque pariatur laborum quaerat consequuntur! Praesentium natus
-                commodi quibusdam sapiente minus esse magnam cumque obcaecati at
-                consequatur.
-              </p>
+            <div className="valign-wrapper justify-content-center">
+              <img src={logo} alt="Expensicon Logo" />
+              <span className="center-align landing-brand-heading">
+                Expensicon
+              </span>
             </div>
-          </div>
-        </div>
-
-        <div className="pimg2">
-          <div className="ptext">
-            <span className="border">Image Two Text</span>
           </div>
         </div>
 
         <div className="section section-dark">
           <div className="row">
             <div className="col m6 offset-m3">
-              <h2>Section Two</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad
-                provident laudantium exercitationem, dolorem sint soluta
+              <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+              <p className="section-text">
+                Ad provident laudantium exercitationem, dolorem sint soluta
                 quibusdam in iure, fugit magni distinctio rem expedita ab unde
                 odio quam voluptate nam voluptates, iusto doloribus dolorum
                 molestiae quidem! Odio harum omnis sint necessitatibus sed
@@ -70,29 +46,24 @@ class Landing extends Component {
         </div>
 
         <div className="pimg3">
-          <div className="ptext">
-            <span className="border">Image Three Text</span>
-          </div>
+          <div className="ptext">Morbi nec vestibulum</div>
         </div>
 
-        <div className="section section-dark">
+        <div className="section section-light">
           <div className="row">
             <div className="col m6 offset-m3">
-              <h2>Section Three</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad
-                provident laudantium exercitationem, dolorem sint soluta
-                quibusdam in iure, fugit magni distinctio rem expedita ab unde
-                odio quam voluptate nam voluptates, iusto doloribus dolorum
-                molestiae quidem! Odio harum omnis sint necessitatibus sed
-                corporis maiores pariatur nisi minima. Doloremque rerum ipsum
-                cumque totam nulla adipisci ducimus tempora ab ut voluptatum
-                minima dolore aperiam, possimus officiis accusamus repellat
-                tenetur, animi at expedita nam! Eligendi ducimus commodi quas
-                possimus, nemo aliquam vero corrupti dolor optio? Animi, quia
-                neque pariatur laborum quaerat consequuntur! Praesentium natus
-                commodi quibusdam sapiente minus esse magnam cumque obcaecati at
-                consequatur.
+              <h2>Nam ex ex, egestas nec lectus condimentum</h2>
+              <p className="section-text">
+                Efficitur blandit risus. Nunc nec urna vehicula, tempus risus
+                at, ultricies nunc. Suspendisse egestas, tellus sed pharetra
+                porttitor, tellus ante scelerisque ligula, consequat
+                sollicitudin elit odio et purus. Vestibulum vel massa eu mi
+                elementum pellentesque a eu augue. Duis volutpat nulla at leo
+                dictum pharetra. Donec vehicula volutpat vehicula. Class aptent
+                taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos. Maecenas vehicula urna egestas ex suscipit
+                pharetra. Ut congue, tellus ut convallis placerat, eros mi
+                finibus enim, ut gravida ligula nisl at sem.
               </p>
             </div>
           </div>
@@ -100,7 +71,19 @@ class Landing extends Component {
 
         <div className="pimg4">
           <div className="ptext">
-            <span className="border">Image Four Text</span>
+            <div className="valign-wrapper justify-content-center">
+              <img src={logo} alt="Expensicon Logo" />
+              <span className="center-align landing-brand-heading">
+                Expensicon
+              </span>
+            </div>
+          </div>
+          <div className="footer white-text">
+            <div className="footer-row center-align">Expensicon</div>
+            <div className="footer-row center-align">Hamilton, Ontario</div>
+            <div className="footer-row center-align">289-775-6046</div>
+            <div className="footer-row center-align">support@expensicon.ca</div>
+            <p></p>
           </div>
         </div>
       </div>
